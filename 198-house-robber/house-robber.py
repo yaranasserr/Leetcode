2359d5a412e176ae110d4241cf33b4ca45cdf686
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2024-12-01 00:21:13 +0200

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.10%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for i in nums:
+            temp = max(rob2, rob1 + i)  # Decide whether to rob the current house
+            rob1 = rob2                # Update rob1 to the value of rob2
+            rob2 = temp                # Update rob2 to the new max value
+        return rob2
+        
