22518c73bd74e043e1b0564c3ba1ef063f2247b6
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2024-12-16 14:20:06 +0200

    Update file 12/16/2024, 2:20:06 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -3,7 +3,8 @@ class Solution:
         index_map = {num: i for i, num in enumerate(nums)} # key -> number , value-> index
         dp = []
         for num in nums :
-            index=bisect_left(dp,num)
+            index=bisect_left(dp,num) 
+            # bisect_left(sortedlist, element) returns the index in which element to be inserted , keeping the array sorted
              
             if index < len(dp):
                 dp[index]=num
