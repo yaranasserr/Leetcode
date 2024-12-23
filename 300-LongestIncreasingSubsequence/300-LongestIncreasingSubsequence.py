b40caf20d290807a6009233394c814788c6eab38
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2024-12-23 16:18:37 +0200

    Update file 12/23/2024, 4:18:37 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # dp and binary search : o(n log n) 
         index_map = {num: i for i, num in enumerate(nums)} # key -> number , value-> index
         dp = []
         for num in nums :
@@ -12,3 +13,31 @@ class Solution:
                 dp.append(num)
                 
         return  len(dp)
+
+        # manual binary serach
+""" 
+indices = [None for x in range(len(nums)+1)]
+length = 0 
+for i, num in enumerate(nums):
+    updatelength =self.binarysearch(self,length,indices,nums,num)
+    indices[updatelength] = i
+    length = max(length,updatedlength)
+
+def binarysearch(startindex,endindex,indices,nums,num):
+    # base case 
+    if startindex > endindex:
+        return startindex 
+
+    middleindex = (startindex+endindex) //2
+    if nums[indices[middleindex]] < num:
+        startindex = middleindex+1
+    else:
+        end index = middleindex-1
+
+    return self.binarysearch(startindex,endindex,indices,nums,num)
+
+
+
+
+
+"""
