ed41832a2f594502289e8114e72a1aa701f50b2d
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2024-12-26 16:33:17 +0200

    Update file 12/26/2024, 4:33:17 PM

--- 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.py
+++ 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.py
@@ -16,20 +16,20 @@
 #         root.left = self.buildTree(postorder[1:mid+1],inorder[:mid])
 #         root.right = self.buildTree(postorder[mid+1:],inorder[mid+1:])
 #         return root 
-
+# build the right subtree first recusion n^2
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         if not inorder or not postorder:
             return None
 
-        # The root is the last element in postorder traversal
-        root = TreeNode(postorder[-1])
-        
-        # Find the index of the root in inorder traversal
-        mid = inorder.index(postorder[-1])
+        root = TreeNode(postorder.pop())
+        mid = inorder.index(root.val)
+        root.right = self.buildTree(inorder[mid+1:],postorder)
+        root.left = self.buildTree(inorder[:mid],postorder)
+        return root 
 
-        # Build the left and right subtrees recursively
-        root.left = self.buildTree(inorder[:mid], postorder[:mid])
-        root.right = self.buildTree(inorder[mid+1:], postorder[mid:-1])
+        # # Build the left and right subtrees recursively
+        # root.left = self.buildTree(inorder[:mid], postorder[:mid])
+        # root.right = self.buildTree(inorder[mid+1:], postorder[mid:-1])
         
-        return root
+        # return root
