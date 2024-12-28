ddb1643088eebee4cdbc0211a2262fa0de9f6608
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2024-12-29 00:20:16 +0200

    Time: 0 ms (100.00%) | Memory: 17.7 MB (9.73%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(cur, num):
+            if not cur:
+                return 0  # Return 0 for null nodes
+            num = num * 10 + cur.val
+            if not cur.left and not cur.right:  # Proper leaf node check
+                return num
+            # Sum the numbers from left and right subtrees
+            return dfs(cur.left, num) + dfs(cur.right, num)
+
+        return dfs(root, 0)
