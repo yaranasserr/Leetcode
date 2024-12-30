2ea0a3a1ca8b2433cf7a229d47409f49c5950a67
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2024-12-31 00:09:28 +0200

    Update file 12/31/2024, 12:09:28 AM

--- 162-FindPeakElement/162-FindPeakElement.py
+++ 162-FindPeakElement/162-FindPeakElement.py
@@ -1,4 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        maximum = max(nums)
-        return nums.index(maximum)
+        left, right = 0, len(nums) - 1
+            
+        while left < right:
+            mid = (left + right) // 2
+                
+                # Compare mid with its right neighbor
+            if nums[mid] > nums[mid + 1]:
+                    # Peak is in the left half (including mid)
+                    right = mid
+            else:
+                    # Peak is in the right half
+                left = mid + 1
+            
+            # left and right will converge to the peak
+        return left
