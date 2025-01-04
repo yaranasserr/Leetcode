71ae6fde983766eae9ace14ce2a03c7c0c84fee3
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-01-04 14:45:11 +0200

    Create file

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        for i in range(len(nums)):
+            if i > farthest:  
+                return False
+            farthest = max(farthest, i + nums[i])  
+        return True if farthest >= len(nums) - 1 else False
+
+
+
+
+
+
+            
+
+        
