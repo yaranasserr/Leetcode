790eee0db48e8124d5ea6a209beaceffd264db5e
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-01-07 14:50:24 +0200

    Update file 1/7/2025, 2:50:24 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,7 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        y=str(x)
-        if(y[::-1]==y):
-             return True
+        s=str(x)
+        l  , r = 0 , len(s)-1
+        while l<=r :
+            if s[l] != s[r]:
+                return False 
+            l,r = l+1 ,r-1
+        return True  
+    
              
           
