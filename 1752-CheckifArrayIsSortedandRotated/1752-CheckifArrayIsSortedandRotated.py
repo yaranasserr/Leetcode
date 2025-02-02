f57c44bd2a0bd8152158284fbfdba9e89b6ffa6b
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-02-02 13:50:42 +0200

    Update file 2/2/2025, 1:50:42 PM

--- 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.py
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.py
@@ -2,13 +2,39 @@ from typing import List
 
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        count_drops = 0
+        # two pointers 
         n = len(nums)
+        count = 1
+        if n == 1:
+            return True 
+            
+        for i in range(1,2*n):
+            if nums[(i-1)%n] <= nums[i%n]:
+                count +=1
+            else :
+                count = 1 
+
+            if count == n :
+                return True 
+        return False 
+        # dq = deque(nums)
+    
+        # for _ in range(len(nums)):  
+        #     if list(dq) == sorted(dq): 
+        #         return True
+        #     dq.append(dq.popleft())  
         
-        for i in range(n):
-            if nums[i] > nums[(i + 1) % n]:  
-                count_drops += 1
-                if count_drops > 1: 
-                    return False
+        # return False
+
+
+            
+        # count_drops = 0
+        # n = len(nums)
         
-        return True
+        # for i in range(n):
+        #     if nums[i] > nums[(i + 1) % n]:  
+        #         count_drops += 1
+        #         if count_drops > 1: 
+        #             return False
+        
+        # return True
