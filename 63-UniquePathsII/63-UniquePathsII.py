654b74bbb3fae4c80e332c5aff912ca6c75f6798
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-02-10 18:14:08 +0200

    File updated at 2/10/2025, 6:14:08 PM

--- 63-UniquePathsII/63-UniquePathsII.py
+++ 63-UniquePathsII/63-UniquePathsII.py
@@ -1,25 +1,24 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # obstacle -> 1
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        from functools import lru_cache
+        dp = {}
+        m= len(obstacleGrid)
+        n=len(obstacleGrid[0])
 
-        @lru_cache(None)
-
-        def find(r,c):
+        def dfs(r,c):
             if not obstacleGrid:
                 return 
             if obstacleGrid[0][0] == 1:
                 return 0 
-                
-            if r == m-1 and c == n -1 :
-                return 1
-
-            if r >= m or c >= n or obstacleGrid[r][c] == 1 or obstacleGrid[m-1][n-1] == 1  :
-                return 0
+            if r== m-1 and c == n-1:
+                return 1 
+            # base cases 
+            if r >= m or c >= n or obstacleGrid[r][c] == 1 or obstacleGrid[m-1][n-1] == 1:
+                return 0 
+            # memo
+            if (r,c) in dp :
+                return dp[(r,c)]
             
-            return find(r + 1, c) + find(r, c + 1)
+            dp[(r,c)] = dfs(r+1,c) +dfs(r,c+1)
+            return dp[(r,c)]
 
-        return find(0,0)
-        
+        return dfs(0,0)
