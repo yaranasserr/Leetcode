bd66b9f9c220065076bd04b89f3ab954857c66d6
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-02-13 01:14:12 +0200

    File updated at 2/13/2025, 1:14:10 AM

--- 205-IsomorphicStrings/205-IsomorphicStrings.py
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -5,22 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        mapST , mapTS  ={} , {} 
-       #  mapST map characters from s to t.
-        #  mapTS map characters from t to s.
-
-          # iteration in two arrays
-
-        for c1 ,c2 in zip(s,t):
-            if ((c1 in mapST and mapST[c1] != c2 ) or (c2 in mapTS and mapTS[c2] != c1 )):
+        mapst,mapts = {} ,{}
+        for c1 , c2 in zip(s,t):
+            if (c1 in mapst and mapst[c1]!= c2) or (c2 in mapts and mapts[c2]!= c1):
                 return False 
-            # Add the mapping from c1 to c2 in mapST
-            mapST[c1] = c2
-
-            # Add the mapping from c2 to c1 in mapTS
-            mapTS[c2] = c1
+            mapst[c1]=c2
+            mapts[c2]=c1 
 
         return True 
+     
+        
+
+
            
 
 
