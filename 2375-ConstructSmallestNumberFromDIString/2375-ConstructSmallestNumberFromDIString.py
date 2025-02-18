d59e489406bd0c7637c92d31cc53deac453ed35c
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-02-18 23:29:59 +0200

    File updated at 2/18/2025, 11:29:59 PM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.py
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.py
@@ -1,32 +1,11 @@
 class Solution:
-    # Check if the current sequence matches the pattern of 'I' and 'D'
-    def check(self, number_sequence: str, pattern: str) -> bool:
-        for index in range(len(pattern)):
-            # Ensure the sequence is in increasing order at 'I' positions
-            if (
-                pattern[index] == "I"
-                and number_sequence[index] > number_sequence[index + 1]
-            ):
-                return False
-            # Ensure the sequence is in decreasing order at 'D' positions
-            elif (
-                pattern[index] == "D"
-                and number_sequence[index] < number_sequence[index + 1]
-            ):
-                return False
-        return True
-
     def smallestNumber(self, pattern: str) -> str:
-        pattern_length = len(pattern)
+        res , stack = [] , []
+        for i in range(len(pattern)+1):
+            stack.append(i+1)
+            while stack and (i==len(pattern) or pattern[i]=="I"):
+                res.append(str(stack.pop()))
 
-        # Generate sequence "123...n+1"
-        number_sequence = "".join(
-            str(num) for num in range(1, pattern_length + 2)
-        )
-
-        # Use permutations generator
-        for permutation in permutations(number_sequence):
-            permutation_str = "".join(permutation)
-            if self.check(permutation_str, pattern):
-                return permutation_str
-        return ""
+        return "".join(res)
+        
+        
