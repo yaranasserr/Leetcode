5547a4825dc5d4531c26b57d13828e89662b54da
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-02-22 19:51:11 +0200

    Create file

---
+++ 1028-RecoveraTreeFromPreorderTraversal/1028-RecoveraTreeFromPreorderTraversal.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import Optional
+
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        dashes = 0 # depth
+        stack = []
+        i = 0 
+        while i < len(traversal):
+            if traversal[i] == "-":
+                dashes += 1
+                i += 1
+            else:
+                j = i 
+                # get all the numbers
+                while j < len(traversal) and traversal[j] != "-":
+                    j += 1
+                val = int(traversal[i:j])
+                node = TreeNode(val)
+
+                while len(stack) > dashes:
+                    stack.pop()
+
+                if stack and not stack[-1].left:
+                    stack[-1].left = node
+                elif stack:
+                    stack[-1].right = node
+
+                stack.append(node)
+                i = j 
+                dashes = 0 
+        return stack[0]
