05f933adc98f1bc360a4bc34502b04d3c24db452
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-03 21:08:13 +0200

    Create file

---
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        smaller = []
+        bigger = []
+        equal = []
+        for  num in nums:
+            if num < pivot :
+                smaller.append(num)
+            elif num > pivot :
+                bigger.append(num)
+            elif num == pivot :
+                equal.append(num)
+
+        res = smaller +equal+bigger
+        return res
+
+        
+
+        
