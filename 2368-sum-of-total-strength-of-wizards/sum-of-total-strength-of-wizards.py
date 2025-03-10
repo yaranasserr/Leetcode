6ed7a2171649479d6fb8f38c2e62814c7a0f9a6e
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-11 00:52:30 +0200

    Time: 496 ms (22.73%) | Memory: 39.4 MB (79.17%) - LeetSync

--- 2368-sum-of-total-strength-of-wizards/sum-of-total-strength-of-wizards.py
+++ 2368-sum-of-total-strength-of-wizards/sum-of-total-strength-of-wizards.py
@@ -1,48 +1,39 @@
 class Solution:
-    def totalStrength(self, strength: List[int]) -> int:
-        
-        MOD = 10**9 + 7
+    # TC: O(n)
+    # SC: O(n)
+    def totalStrength(self, strength):
         n = len(strength)
+        MOD = 1_000_000_007
 
-        # Prefix sum and prefix of prefix sum
-        prefix_sum = [0] * (n + 1)
+        preSum = [0] * (n + 1)
+        prePrefix = [0] * (n + 2)
+
         for i in range(n):
-            prefix_sum[i + 1] = (prefix_sum[i] + strength[i]) % MOD
-
-        prefix_of_prefix = [0] * (n + 2)
+            preSum[i + 1] = (preSum[i] + strength[i]) % MOD
         for i in range(n + 1):
-            prefix_of_prefix[i + 1] = (prefix_of_prefix[i] + prefix_sum[i]) % MOD
+            prePrefix[i + 1] = (prePrefix[i] + preSum[i]) % MOD
 
-        # Next smaller element to the left and right
         left = [-1] * n
-        right = [n] * n
-
         stack = []
         for i in range(n):
-            while stack and strength[stack[-1]] >= strength[i]:
+            while stack and strength[i] <= strength[stack[-1]]:
                 stack.pop()
-            left[i] = stack[-1] if stack else -1
+            left[i] = -1 if not stack else stack[-1]
             stack.append(i)
 
+        right = [n] * n
         stack = []
         for i in range(n - 1, -1, -1):
-            while stack and strength[stack[-1]] > strength[i]:
+            while stack and strength[i] < strength[stack[-1]]:
                 stack.pop()
-            right[i] = stack[-1] if stack else n
+            right[i] = n if not stack else stack[-1]
             stack.append(i)
 
-        # Calculate the total strength
-        res = 0
+        ans = 0
         for i in range(n):
-            l, r = left[i], right[i]
-            total_left = (prefix_of_prefix[i + 1] - prefix_of_prefix[l + 1]) % MOD
-            total_right = (prefix_of_prefix[r + 1] - prefix_of_prefix[i + 1]) % MOD
-            count_left = i - l
-            count_right = r - i
+            left_contrib = (prePrefix[right[i] + 1] - prePrefix[i + 1]) * (i - left[i]) % MOD
+            right_contrib = (prePrefix[i + 1] - prePrefix[left[i] + 1]) * (right[i] - i) % MOD
+            ans += (left_contrib + MOD - right_contrib) * strength[i] % MOD
+            ans %= MOD
 
-            contribution = (strength[i] * (total_right * count_left - total_left * count_right)) % MOD
-            res = (res + contribution) % MOD
-
-        return res
-
-        
+        return ans
