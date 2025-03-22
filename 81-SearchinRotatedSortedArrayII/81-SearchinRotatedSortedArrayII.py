310437d4725080ee38d379dd8428b2c6a9f5332f
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-22 13:16:13 +0200

    Create file

---
+++ 81-SearchinRotatedSortedArrayII/81-SearchinRotatedSortedArrayII.py
@@ -0,0 +1,28 @@
+// Last updated: 3/22/2025, 1:16:12 PM
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        nums= set(nums)
+        nums= list(nums)
+        
+        l,r = 0 , len(nums)-1
+
+        while l <= r :
+            m = (l+r)//2
+            if nums[m] == target :
+                return True 
+
+            if nums[l] <= nums[m]:
+                if nums[l]<= target <= nums[m] :
+                    r = m-1
+                else:
+                    l = m+1
+            else:
+                if nums[m]>= target >= nums[r]:
+                    l = m +1
+                else:
+                    r = m-1 
+                    
+
+
+        return False 
+        
