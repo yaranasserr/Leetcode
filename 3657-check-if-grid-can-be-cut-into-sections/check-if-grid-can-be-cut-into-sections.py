3f757249c0974b964d1496849fa7884abc04cdfb
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-25 16:36:30 +0200

    Time: 497 ms (48.65%) | Memory: 83.6 MB (46.62%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,52 @@
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        x = []
+        y=[]
+
+        for x1,y1,x2,y2 in rectangles :
+            x.append((x1,x2))
+            y.append((y1,y2))
+
+
+        x.sort()
+        y.sort()
+
+        def count_nonoverlappingintervals(interval):
+            count = 0 
+            prevend = - 1
+
+            for start , end in interval:
+                if prevend <= start :
+                    count +=1 
+                prevend = max(end,prevend)
+
+
+            return count 
+
+
+
+
+        return max(count_nonoverlappingintervals(x),count_nonoverlappingintervals(y))>= 3 
+
+
+        # def count_nonoverlappingintervals(interval):
+        #     interval.sort(key=lambda x:x[0])
+        #     prevend = interval[0][1]
+
+        #     count = 1
+        #     for start , end in interval[1:]:
+        #         if start >= prevend :
+        #             count +=1 
+
+        #     return count 
+
+        # # non overlapping intervals 
+
+        # if count_nonoverlappingintervals(xintervals) >= 3 or count_nonoverlappingintervals(yintervals) >= 3 : 
+
+        #     return True 
+
+        # return False 
+
+        
+        
