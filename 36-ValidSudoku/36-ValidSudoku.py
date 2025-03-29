a58bf8466075a68d021e4c0444497fc1f168505b
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-29 23:54:13 +0200

    File updated at 3/29/2025, 11:54:13 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,33 +1,40 @@
-# Last updated: 3/29/2025, 11:54:06 PM
+# Last updated: 3/29/2025, 11:54:13 PM
+import numpy as np
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        res =  []
-        top=left = 0 
-        bottom = len(matrix)
-        right = len(matrix[0]) # last col
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+      
 
-        while left < right and top < bottom :
-            # top from left to right
-            for i in range(left,right):
-                res.append(matrix[top][i])
-            top+=1
-            # right col from top to bottom 
-            for i in range(top,bottom):
-                res.append(matrix[i][right-1])
-            right -=1 
-
-            if not (left<right and top < bottom):
-                break
-
-            # bottom (right to left)
-            for i in range(right-1,left-1,-1):
-                res.append(matrix[bottom-1][i])
-            bottom -=1 
-
-            # left col from bottom to top 
-            for i in range(bottom-1,top-1,-1):
-                res.append(matrix[i][left])
-            left +=1
-
-        return res 
+    #First Transpose matrix
+   
         
+    
+        n=len(matrix)
+        startC=0
+        for r in range(n):
+            for c in range(startC,n):
+                matrix[r][c] , matrix[c][r] = matrix[c][r] , matrix[r][c]
+            startC+=1
+            
+            
+            
+         #second swap the columns
+       
+        nColSwap=n//2
+        for c in range(nColSwap):
+            for r in range(n):
+                 matrix[r][c],matrix[r][n-c-1]=matrix[r][n-c-1],matrix[r][c]
+                    
+
+            
+        
+      
+          
+       
+     
+     
+       
+          
+            
