fbfc3c2a62353e2bcb083735ed87f3436e370366
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-29 15:54:03 +0200

    Create file

---
+++ 1656-DesignanOrderedStream/1656-DesignanOrderedStream.py
@@ -0,0 +1,22 @@
+# Last updated: 3/29/2025, 3:54:02 PM
+class OrderedStream:
+    def __init__(self, n):
+        self.stream = [None] * n  
+        self.ptr = 0  
+
+    def insert(self, idKey, value):
+        self.stream[idKey - 1] = value  
+        result = []
+
+        while self.ptr < len(self.stream) and self.stream[self.ptr]:
+            result.append(self.stream[self.ptr])
+            self.ptr += 1  
+
+        return result
+
+        
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
