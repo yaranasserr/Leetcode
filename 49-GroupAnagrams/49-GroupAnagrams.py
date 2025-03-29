185a0acdae3b36d45b9384bd3ec0886a26a3fcd0
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-29 12:29:12 +0200

    File updated at 3/29/2025, 12:29:12 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,12 +1,36 @@
+# Last updated: 3/29/2025, 12:29:12 PM
+from typing import List
+
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
-        for w in strs:
-            key=''.join(sorted(w))
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def backtrack(i):
+            # i index 
+            if i == len(s):
+                res.append(" ".join(cur))
+                return
 
-            if key not in anagrams :
-                anagrams[key] = []
-            anagrams[key].append(w)
+            for j in range(i,len(s)):
+                w=s[i:j+1]
+                if w in wordDict :
+                    cur.append(w)
+                    backtrack(j+1)
+                    cur.pop()
 
-        return list(anagrams.values())
-        
+        cur=[]
+        res=[]
+        backtrack(0)
+        return res
+        # def backtrack(start, path):
+        #     if start == len(s):
+        #         final.append(" ".join(path))
+        #         return
+
+        #     for word in wordDict:
+        #         if s[start:start+len(word)] == word:
+        #             backtrack(start + len(word), path + [word])
+
+        # final = []
+        # backtrack(0, [])
+        # return final
+
+    # O(2 ** n)
