8c7cce8ab2eb92eb2821235233bce7e8bafa2260
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-29 23:57:05 +0200

    File updated at 3/29/2025, 11:57:05 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,59 +1,25 @@
-# Last updated: 3/29/2025, 11:55:45 PM
+# Last updated: 3/29/2025, 11:57:05 PM
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l = 0 
-        r = len(nums) -1 
+    def findMin(self, nums: List[int]) -> int:
+        l , r = 0 , len(nums)-1
+        res = nums[0]
 
-        while l<= r :
-                
-            mid=(l+r)//2
-            if nums[mid] == target :
-                return mid 
-            # determine which half is sorted 
-            # left half is sorted
-            if nums[l] <= nums[mid]:
-                # target in left half 
-                if nums[l]<= target <= nums[mid]:
-                    r = mid -1 
-                else:
-                    l = mid+1  # target in right half 
+        while l<=r :
+            # array is sorted 
+            if nums[l]<nums[r]:
+                res = min(res,nums[l])
+                break 
 
-            # right half is sorted :
-
+            # mid in left portion -> search in the right
+            mid = (l+r)//2
+            res = min(res,nums[mid]) 
+            if nums[mid]>= nums[l]:
+                l= mid+1 
             else:
-                if nums[mid] <= target <= nums[r]:
-                    l = mid+1
-                else:
-                    r = mid -1 
-        return -1 
+                r = mid -1 
+
+        return res
 
 
-
-
-
-
-            # if target < mid -> search in right 
-            # if nums[mid] >target  and nums[l] >= nums[r]:
-            #     l = mid +1
-            # elif nums[mid] < target and nums[l] >= nums[r]:
-
-            #     r = mid -1 
-            # elif  nums[l]<nums[r] and nums[mid] < target :
-            #     l = mid+1 
-            # elif  nums[l]<nums[r] and nums[mid] >target :
-            #     r = mid -1 
-
-
-            # else:
-            #     return mid
-
-"""
-[4,5,6,7,0,1,2]
-l = 0 , r = 6 mid = 4 , nums= 7 
-if nums[mid] >target
-l= 4 r = 6 mid= 5 nums = 1
-if nums[mid] >target 
-"""
-        
 
         
