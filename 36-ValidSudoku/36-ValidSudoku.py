2232ef14dfdcee6a88f4017714b4d7bbbf9b81d6
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-29 23:55:25 +0200

    File updated at 3/29/2025, 11:55:24 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,23 +1,40 @@
-# Last updated: 3/29/2025, 11:55:10 PM
-from typing import List
-
+# Last updated: 3/29/2025, 11:55:24 PM
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) <= 1:
-            return intervals
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+
+        l= 0
+        r = len(intervals) -1
+    
+
+        while l <= r :
+            mid = (l+r )//2
+
+            if intervals[mid][0] < newInterval[0]:
+                l = mid +1
+            else:
+                r = mid -1 
+
+        # mid is where the new interval will be places 
+
+        intervals.insert(l,newInterval)
         
-        # Sort intervals by start time
-        intervals.sort(key = lambda i :i[0]) # sort by start of interval 
+        
+        # merge intervals
         res = [intervals[0]]
 
-        for start,end in intervals[1:]:
+        for start , end in intervals[1:]:
             lastend = res[-1][1]
 
-            if start <= lastend :
-                res[-1][1] = max(lastend,end)
+            if start <= lastend:
+                res[-1][1]=max(end,lastend)
             else:
                 res.append([start,end])
 
         return res
 
-           
+        
+
+        
+        
