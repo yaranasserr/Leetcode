1080f1159466c1ccfbb6df2b19b6ba57d956b14c
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-29 17:58:44 +0200

    Time: 2 ms (59.62%) | Memory: 19.7 MB (40.95%) - LeetSync

---
+++ 57-insert-interval/insert-interval.py
@@ -0,0 +1,39 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+
+        l= 0
+        r = len(intervals) -1
+    
+
+        while l <= r :
+            mid = (l+r )//2
+
+            if intervals[mid][0] < newInterval[0]:
+                l = mid +1
+            else:
+                r = mid -1 
+
+        # mid is where the new interval will be places 
+
+        intervals.insert(l,newInterval)
+        
+        
+        # merge intervals
+        res = [intervals[0]]
+
+        for start , end in intervals[1:]:
+            lastend = res[-1][1]
+
+            if start <= lastend:
+                res[-1][1]=max(end,lastend)
+            else:
+                res.append([start,end])
+
+        return res
+
+        
+
+        
+        
