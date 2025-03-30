d7b993bd74ed7f32a8e42c0555b90711de995071
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-30 16:37:17 +0200

    Time: 3 ms (91.15%) | Memory: 17.6 MB (85.21%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,35 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # two pointers
+        # Precompute the last occurrence of each character in s
+        last ={c:i for i,c in enumerate(s)}
+        #{'a': [2], 'b': [3], 'c': [5]}
+    
+        #track the farthest last occurrence 
+        start = 0 
+        end = 0
+        res = []
+        for i,c in enumerate(s):
+            end = max(end,last[c]) 
+            if i == end :
+                res.append(end-start+1)
+                start =i+1
+
+        return res
+
+
+
+
+
+ 
+       
+
+        
+
+            
+
+
+            
+
+
+        
