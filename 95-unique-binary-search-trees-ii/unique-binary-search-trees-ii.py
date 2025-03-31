923f6656747f9e0325c7cf57c19fe3f6a9fb4731
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-31 17:13:07 +0200

    Time: 3 ms (62.06%) | Memory: 19.2 MB (7.28%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+
+        def gen(left,right):
+            if left == right :
+                return [TreeNode(left)]
+
+            if left > right :
+                return [None]
+
+            res = []
+            #any val can be the root 
+            for val in range(left,right+1):
+                root =TreeNode(val)
+
+                for lefttree in gen(left,val-1): # left subtree 
+                    for righttree in gen(val+1,right):
+                        root = TreeNode(val,lefttree,righttree)
+                        res.append(root)
+
+            return res
+
+
+
+        return gen(1,n)
+
+
+        
