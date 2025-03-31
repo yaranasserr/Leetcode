8b252a152d73ff7a46e6ce2200672c980d6ad55a
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-31 17:50:09 +0200

    File updated at 3/31/2025, 5:50:09 PM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,35 +1,25 @@
-# Last updated: 3/31/2025, 5:13:05 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Last updated: 3/31/2025, 5:50:09 PM
 class Solution:
-    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        # sliding window
+        length = float("inf")
+        l=0 
+        summ= 0 
 
-        def gen(left,right):
-            if left == right :
-                return [TreeNode(left)]
+        
 
-            if left > right :
-                return [None]
+        for r in range(len(nums)):
+        
+            summ += nums[r]
 
-            res = []
-            #any val can be the root 
-            for val in range(left,right+1):
-                root =TreeNode(val)
+            while summ >= target :
+                length = min(length,r-l+1)
+                print(length)
+                summ -= nums[l]
+                l+=1
 
-                for lefttree in gen(left,val-1): # left subtree 
-                    for righttree in gen(val+1,right):
-                        root = TreeNode(val,lefttree,righttree)
-                        res.append(root)
+        return length if length != float("inf") else 0
 
-            return res
-
-
-
-        return gen(1,n)
-
+        
 
         
