83c08bce6b3ca34535b420be66dbdc64d58c6a04
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-03-31 21:31:34 +0200

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.37%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,44 +3,106 @@ from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # multisource bfs 
-        rows = len(grid)
-        cols = len(grid[0])
-        visit = set()
-        q= deque()
-        time = 0 
+        m = len(grid)
+        n=len(grid[0])
         fresh = 0 
-        directions = [[0, 1],  # Right
-                      [0, -1], # Left
-                      [1, 0],  # Down
-                      [-1, 0]] # Up 
-        #count the fresh and add rotten in queue
-        for r in range(rows):
-            for c in range(cols):
+        time = 0 
+        visit=set()
+        q=deque()
+        directions=[[1,0],[-1,0],[0,1],[0,-1]]
+        # count the fresh
+        for r in range(m):
+            for c in range(n):
                 if grid[r][c] == 1:
                     fresh +=1
-                if grid[r][c] == 2:
+        # put the rotten in the queue 
+                if grid[r][c]==2:
                     q.append([r,c])
 
-        # bfs from the rotten 
-        while q  and fresh >0:
+        while q and fresh > 0 :
             for i in range(len(q)):
-                r ,c = q.popleft()
+                r,c = q.popleft()
                 for dr , dc in directions :
-                    row = r +dr 
-                    col = c +dc 
-
-                    # bound 
-                    if (row < 0 or row >= rows or col <0 or col >= cols or grid[row][col] != 1):
+                    row = r +dr
+                    col = c +dc
+                    if row <0 or col<0 or row>= m or col>=n or grid[row][col]!=1:
                         continue
-                    grid[row][col] = 2
-                    q.append([row, col])
-
-
+                    grid[row][col] =2 
+                    q.append([row,col])
                     fresh -=1 
             time+=1
 
 
-        return time if fresh == 0 else -1 
+
+        return time if fresh == 0 else -1
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # multisource bfs 
+        # rows = len(grid)
+        # cols = len(grid[0])
+        # visit = set()
+        # q= deque()
+        # time = 0 
+        # fresh = 0 
+        # directions = [[0, 1],  # Right
+        #               [0, -1], # Left
+        #               [1, 0],  # Down
+        #               [-1, 0]] # Up 
+        # #count the fresh and add rotten in queue
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if grid[r][c] == 1:
+        #             fresh +=1
+        #         if grid[r][c] == 2:
+        #             q.append([r,c])
+
+        # # bfs from the rotten 
+        # while q  and fresh >0:
+        #     for i in range(len(q)):
+        #         r ,c = q.popleft()
+        #         for dr , dc in directions :
+        #             row = r +dr 
+        #             col = c +dc 
+
+        #             # bound 
+        #             if (row < 0 or row >= rows or col <0 or col >= cols or grid[row][col] != 1):
+        #                 continue
+        #             grid[row][col] = 2
+        #             q.append([row, col])
+
+
+        #             fresh -=1 
+        #     time+=1
+
+
+        # return time if fresh == 0 else -1 
 
 
