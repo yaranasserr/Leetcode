5b20164afdedcc92aae5ec3b4873be95498f9555
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-04-11 15:52:29 +0200

    Time: 923 ms (12.01%) | Memory: 17.8 MB (54.99%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        sym = 0 
+        for i in range(low, high + 1):
+            digits = list(str(i))
+            length = len(digits)
+
+            # Only consider even-digit numbers
+            if length % 2 == 0:
+                half = length // 2
+                first_half = sum(int(x) for x in digits[:half])
+                second_half = sum(int(x) for x in digits[half:])
+                
+                if first_half == second_half:
+                    sym += 1
+
+        return sym
