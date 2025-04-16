16fc318b825e1142b89853de42e23cb8f9f20bb4
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-04-16 16:42:19 +0200

    Time: 120 ms (74.11%) | Memory: 34.2 MB (98.71%) - LeetSync

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        # sliding window 
+
+        n = len(nums)
+        left = 0
+        pairs = 0
+        freq = defaultdict(int)
+        res = 0
+
+        for right in range(n):
+            pairs += freq[nums[right]]
+            freq[nums[right]] += 1
+
+            while pairs >= k:
+                res += n - right  # all subarrays [left, right] to [left, n-1] are good
+                freq[nums[left]] -= 1
+                pairs -= freq[nums[left]]
+                left += 1
+
+        return res
