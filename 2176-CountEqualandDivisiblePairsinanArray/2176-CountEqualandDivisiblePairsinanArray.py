e482a80bf1285869dafc0009736f6b58becc6a67
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-04-17 11:38:33 +0200

    Create file

---
+++ 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.py
@@ -0,0 +1,20 @@
+# Last updated: 4/17/2025, 11:38:33 AM
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        #0 <= i < j < n
+        # nums[i] == nums[j]
+        # i * k % k == 0
+        n = len(nums)
+        res = []
+        pairs = 0 
+        for i in range(n):
+            for j in range(i,n):
+                if nums[i] == nums[j] and ((i*j)%k) == 0 and i <j :
+                    
+                    pairs +=1
+
+        return pairs 
+
+
+
+        
