0f1ac240d9220bf454aa36511eeabd78b84808e8
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-04-18 15:32:48 +0200

    Create file

---
+++ 38-CountandSay/38-CountandSay.py
@@ -0,0 +1,28 @@
+# Last updated: 4/18/2025, 3:32:47 PM
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        
+        def intfreq(s):
+            res = []
+            i = 0
+            while i < len(s):
+                count = 1
+                while i + 1 < len(s) and s[i] == s[i + 1]:
+                    count += 1
+                    i += 1
+                res.append([count, int(s[i])])
+                i += 1
+            return res
+
+        def build_from_pairs(pairs):
+            result = ""
+            for count, digit in pairs:
+                result += str(count) + str(digit)
+            return result
+
+        current = "1"
+        for _ in range(n - 1):
+            pairs = intfreq(current)
+            current = build_from_pairs(pairs)
+
+        return current
