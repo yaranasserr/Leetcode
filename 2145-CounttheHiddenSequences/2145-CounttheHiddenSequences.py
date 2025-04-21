f3a5dda00c6651c1ee75ab61ecf61ba9c5eef081
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-04-21 18:52:28 +0200

    Create file

---
+++ 2145-CounttheHiddenSequences/2145-CounttheHiddenSequences.py
@@ -0,0 +1,13 @@
+# Last updated: 4/21/2025, 6:52:28 PM
+class Solution:
+    def numberOfArrays(
+        self, differences: List[int], lower: int, upper: int
+    ) -> int:
+        x = y = cur = 0
+        for d in differences:
+            cur += d
+            x = min(x, cur)
+            y = max(y, cur)
+            if y - x > upper - lower:
+                return 0
+        return (upper - lower) - (y - x) + 1
