7d7ac104d36a21afb9f83157b8cba668d3dd7da3
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-04-25 20:19:11 +0300

    Create file

---
+++ 2845-CountofInterestingSubarrays/2845-CountofInterestingSubarrays.py
@@ -0,0 +1,14 @@
+# Last updated: 4/25/2025, 8:19:10 PM
+class Solution:
+    def countInterestingSubarrays(
+        self, nums: List[int], modulo: int, k: int
+    ) -> int:
+        n = len(nums)
+        cnt = Counter([0])
+        res = 0
+        prefix = 0
+        for i in range(n):
+            prefix += 1 if nums[i] % modulo == k else 0
+            res += cnt[(prefix - k + modulo) % modulo]
+            cnt[prefix % modulo] += 1
+        return res
