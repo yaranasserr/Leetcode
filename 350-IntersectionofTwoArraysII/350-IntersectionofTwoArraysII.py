504f1cc2cb03f7d683169202e975156435411a45
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-05-15 20:28:48 +0300

    File updated at 5/15/2025, 8:28:48 PM

--- 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.py
+++ 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.py
@@ -1,9 +1,29 @@
+# Last updated: 5/15/2025, 8:28:48 PM
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans=[]
-        for i in nums1:
-            if i in nums2:
-                ans.append(i)
-                nums2.remove(i)
-        return ans
-        
+    def intersect(self, tasks1: List[int], tasks2: List[int]) -> List[int]:
+        # ans=[]
+        # for i in nums1:
+        #     if i in nums2:
+        #         ans.append(i)
+        #         nums2.remove(i)
+        # return ans
+        def freq(arr):
+            c= {}
+            for n in arr:
+                if n in c:
+                    c[n] += 1
+                else:
+                    c[n] = 1
+            return c
+            # for n in arr :
+            #     c[n] = 1+c.get(n,0)
+            # return c
+
+        res = []
+        c1 = freq(tasks1)
+        c2= freq(tasks2)
+
+        for num in c1 :
+            if num in c2 :
+                res += [num] * min(c1[num],c2[num])
+        return res
