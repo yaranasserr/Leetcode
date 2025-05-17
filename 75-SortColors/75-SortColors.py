e1ee174d59454bb0fbf98fa21fc8fcba1157b6a4
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-05-17 15:31:59 +0300

    Create file

---
+++ 75-SortColors/75-SortColors.py
@@ -0,0 +1,17 @@
+# Last updated: 5/17/2025, 3:31:58 PM
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Count occurrences of 0, 1, and 2
+        freq = [0] * 3
+        for n in nums:
+            freq[n] += 1
+
+        # Overwrite nums in-place
+        index = 0
+        for color in range(3):
+            for _ in range(freq[color]):
+                nums[index] = color
+                index += 1
