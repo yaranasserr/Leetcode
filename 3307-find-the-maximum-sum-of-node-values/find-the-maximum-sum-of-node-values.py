8c119a2b2f5e6e709deb922c2e2312392d60f240
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-05-24 14:02:10 +0300

    Time: 32 ms (77.19%) | Memory: 26.6 MB (19.30%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -1,17 +1,18 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        n = len(nums)
-        dp = [[0] * 2 for _ in range(n + 1)]
-        dp[n][1] = 0
-        dp[n][0] = -float('inf')
-        
-        for index in range(n - 1, -1, -1):
-            for isEven in range(2):
-                # Case 1: we perform an operation on this element.
-                performOperation = dp[index + 1][isEven ^ 1] + (nums[index] ^ k)
-                # Case 2: we don't perform operation on this element.
-                dontPerformOperation = dp[index + 1][isEven] + nums[index]
+        # find delta array which = xor array - original array 
+        xor = [n^k for n in nums]
+        delta = [x-n for x,n in zip(xor,nums)]
+        delta.sort(reverse=True)
+        original = sum(nums)
 
-                dp[index][isEven] = max(performOperation, dontPerformOperation)
+        for i in range(0,len(delta)-1,2):
+            gain = delta[i]+delta[i+1]
+            if gain > 0 :
+                original += gain 
+            else:
+                break
+           
+          
+        return original
         
-        return dp[0][1]
