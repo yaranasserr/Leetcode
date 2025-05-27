b1852723b9b1cf3888d30fb47ac4ba08d2ad405f
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-05-27 11:36:32 +0300

    File updated at 5/27/2025, 11:36:31 AM

--- 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.py
+++ 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.py
@@ -1,8 +1,6 @@
-# Last updated: 5/27/2025, 11:35:51 AM
+# Last updated: 5/27/2025, 11:36:31 AM
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        nums = range(1,n+1)
-
-        nums1 = sum([x if x%m != 0 else 0 for x in nums])
-        nums2 = sum([x if x%m == 0 else 0 for x in nums])
-        return nums1-nums2
+    
+        return sum([x if x%m != 0 else 0 for x in range(1,n+1)
+])- sum([x if x%m == 0 else 0 for x in range(1,n+1)])
