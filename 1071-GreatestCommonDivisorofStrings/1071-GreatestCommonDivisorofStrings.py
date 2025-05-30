a8e505dd920dcd67662ad2a83c1baaea0f28469a
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-05-31 00:57:09 +0300

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -0,0 +1,15 @@
+# Last updated: 5/31/2025, 12:57:09 AM
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        occurrences = {}
+        for n in arr :
+            occurrences[n] = 1+occurrences.get(n,0)
+
+
+
+        # occurrences = {item: arr.count(item) for item in set(arr)} # frequency
+        print(set(occurrences.values()))
+      
+        return len(occurrences.values()) == len(set(occurrences.values()))
+            
+        
