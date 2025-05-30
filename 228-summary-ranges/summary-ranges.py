7abf78dfd1b65ae58b76b2d4cb8080fce5a01de1
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-05-30 22:37:52 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.00%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        groups = []  # List of lists to hold number groups
+        group = [nums[0]]  # Start the first group
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1] + 1:
+                group.append(nums[i])
+            else:
+                groups.append(group)
+                group = [nums[i]]  # Start a new group
+        
+        groups.append(group)  # Add the last group
+
+        # Now format the groups
+        res = []
+        for g in groups:
+            if len(g) == 1:
+                res.append(str(g[0]))
+            else:
+                res.append(f"{g[0]}->{g[-1]}")
+        
+        return res
