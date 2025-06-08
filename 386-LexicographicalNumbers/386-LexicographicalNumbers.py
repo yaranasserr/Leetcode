ebc322d45fae4cf4864ee847114103492ed989d9
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-06-08 14:27:38 +0300

    Create 386-LexicographicalNumbers

---
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.py
@@ -0,0 +1,22 @@
+# Last updated: 6/8/2025, 2:27:37 PM
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        def dfs(cur):
+            if cur > n :
+                return 
+
+            res.append(cur)
+            cur *= 10 
+
+            for i in range(10):
+                dfs(cur+i)
+
+
+        for i in range(1,10):
+            dfs(i)
+
+        return res
+
+        
+        
