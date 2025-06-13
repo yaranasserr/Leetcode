aa3971b8b14932830326748dfe37e9f2bbbc8389
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-06-13 12:02:11 +0300

    Time: 668 ms (6.88%) | Memory: 32.6 MB (40.08%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        nums.sort()
+        l , r = 0 , 10**9
+        res = 10**9 
+
+        def isValid(threshold):
+            i,cnt = 0,0 
+            while i < len(nums)-1:
+                if abs(nums[i]-nums[i+1]) <= threshold:
+                    cnt +=1 # valid pairs 
+                    i+=2 # skip those two values 
+                else:
+                    i+=1
+
+                if cnt == p :
+                    return True 
+
+            return False 
+
+        if p == 0 : return 0 
+
+
+
+
+        while l <= r :
+            m = l +(r-l) //2 
+
+            if isValid(m):
+                res = m
+                r = m-1
+            else:
+                l = m+1 # try bigger num
+
+
+        return res 
+
+
+
+        
+        
+      
