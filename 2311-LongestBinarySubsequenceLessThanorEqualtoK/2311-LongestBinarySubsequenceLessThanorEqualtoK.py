e83da838de0da4ca6d25d7e7a130d4a1ee3868ba
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-06-26 14:27:54 +0300

    Create 2311-LongestBinarySubsequenceLessThanorEqualtoK

---
+++ 2311-LongestBinarySubsequenceLessThanorEqualtoK/2311-LongestBinarySubsequenceLessThanorEqualtoK.py
@@ -0,0 +1,14 @@
+# Last updated: 6/26/2025, 2:27:53 PM
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        sm = 0
+        cnt = 0
+        bits = k.bit_length()
+        for i, ch in enumerate(s[::-1]):
+            if ch == "1":
+                if i < bits and sm + (1 << i) <= k:
+                    sm += 1 << i
+                    cnt += 1
+            else:
+                cnt += 1
+        return cnt
