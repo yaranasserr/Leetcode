594b0af1145f42978eeff17232427b4026e324aa
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-06-30 11:00:45 +0300

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.py
@@ -0,0 +1,13 @@
+# Last updated: 6/30/2025, 11:00:45 AM
+class Solution:
+    def findLHS(self, nums):
+        nums.sort()
+        j = 0
+        maxLength = 0
+
+        for i in range(len(nums)):
+            while nums[i] - nums[j] > 1:
+                j += 1
+            if nums[i] - nums[j] == 1:
+                maxLength = max(maxLength, i - j + 1)
+        return maxLength
