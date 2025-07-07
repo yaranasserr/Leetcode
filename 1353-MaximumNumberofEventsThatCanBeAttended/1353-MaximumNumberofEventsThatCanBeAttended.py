781b484c3f5b2543717c488c6db851fcfdb02941
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-07 17:46:39 +0300

    Create 1353-MaximumNumberofEventsThatCanBeAttended

---
+++ 1353-MaximumNumberofEventsThatCanBeAttended/1353-MaximumNumberofEventsThatCanBeAttended.py
@@ -0,0 +1,19 @@
+# Last updated: 7/7/2025, 5:46:37 PM
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        n = len(events)
+        max_day = max(event[1] for event in events)
+        events.sort()
+        pq = []
+        ans, j = 0, 0
+        for i in range(1, max_day + 1):
+            while j < n and events[j][0] <= i:
+                heapq.heappush(pq, events[j][1])
+                j += 1
+            while pq and pq[0] < i:
+                heapq.heappop(pq)
+            if pq:
+                heapq.heappop(pq)
+                ans += 1
+
+        return ans
