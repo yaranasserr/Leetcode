3575b333109f2818e1b2cda1a978e68b64efdd2d
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-08 23:29:15 +0300

    File updated at 7/8/2025, 11:29:15 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,17 +1,5 @@
-# Last updated: 7/8/2025, 11:15:49 PM
+# Last updated: 7/8/2025, 11:29:15 PM
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or numRows >= len(s):
-            return s
-
-        rows = [''] * numRows
-        curr_row = 0
-        going_down = False
-
-        for char in s:
-            rows[curr_row] += char
-            if curr_row == 0 or curr_row == numRows - 1:
-                going_down = not going_down
-            curr_row += 1 if going_down else -1
-
-        return ''.join(rows)
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(ch.lower() for ch in s if ch.isalnum())
+        return s == s[::-1]
