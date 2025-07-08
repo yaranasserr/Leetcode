f063a0cce190ecc31e0c3b1cd0bc66cd6472c405
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-08 23:15:49 +0300

    File updated at 7/8/2025, 11:15:49 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,7 +1,17 @@
-# Last updated: 7/8/2025, 11:14:58 PM
+# Last updated: 7/8/2025, 11:15:49 PM
 class Solution:
-    def strStr(self, haystack, needle):
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i:i+len(needle)] == needle:
-                return i
-        return -1
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        curr_row = 0
+        going_down = False
+
+        for char in s:
+            rows[curr_row] += char
+            if curr_row == 0 or curr_row == numRows - 1:
+                going_down = not going_down
+            curr_row += 1 if going_down else -1
+
+        return ''.join(rows)
