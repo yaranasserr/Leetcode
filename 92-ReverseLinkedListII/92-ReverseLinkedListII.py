c196bf6e63aef49edd0f86e64fbdb452bd7c0caa
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-10 18:10:48 +0300

    File updated at 7/10/2025, 6:10:48 PM

--- 92-ReverseLinkedListII/92-ReverseLinkedListII.py
+++ 92-ReverseLinkedListII/92-ReverseLinkedListII.py
@@ -1,21 +1,41 @@
-# Last updated: 7/10/2025, 6:09:28 PM
+# Last updated: 7/10/2025, 6:10:48 PM
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        # search in first and last cols to locate the targeted row 
+        # then do bs in row 
+        boundaries = {}
+
+        for r in range(m):
+            boundaries[r] = [matrix[r][0], matrix[r][n - 1]]
+
         l = 0 
-        r = len(nums)-1
+        r = n-1
+        row = -1
+
+        for r,(x,y) in boundaries.items():
+            if x <= target <= y :
+                row = r 
+                break 
+
+        if row == -1:
+            return False 
+
+        l = 0 
+        r = n-1 
+        target_row = matrix[row]
 
         while l <= r :
-            mid = (l+r)//2
+            mid = (l+r)//2 
+            if target_row[mid] == target :
+                return True 
 
-            if nums[mid] < target : 
+            elif target_row[mid] < target :
                 l = mid+1
-            elif nums[mid] > target :
+            else:
                 r = mid -1 
-            elif nums[mid] == target :
-                return mid 
 
-        return l 
-
-       
+        return False 
 
         
