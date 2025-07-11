f256b240d8fdaac197447779671ebdb2bc7537e7
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-11 19:28:48 +0300

    File updated at 7/11/2025, 7:28:44 PM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,15 +1,33 @@
-# Last updated: 7/11/2025, 7:21:41 PM
+# Last updated: 7/11/2025, 7:28:44 PM
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        maximum = min(nums)
-        cur = 0 
-        for n in nums :
-            cur+= n 
-            maximum = max(cur,maximum)
-            if cur < 0 :
-                cur = 0 
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        total = sum(nums)
 
-        return maximum
+        def maxsubarry(nums):
+            maximum = float("-inf")
+            cur = 0 
+            for n in nums:
+                cur += n 
+                maximum = max(cur, maximum)
+                if cur < 0:
+                    cur = 0 
+            return maximum
 
-            
+        def minsubarry(nums):
+            minimum = float("inf")
+            cur = 0 
+            for n in nums:
+                cur += n 
+                minimum = min(cur, minimum)
+                if cur > 0:
+                    cur = 0 
+            return minimum
+
+        max_kadane = maxsubarry(nums)
+        min_kadane = minsubarry(nums)
+
+        # Edge case: all numbers are negative
+        if max_kadane < 0:
+            return max_kadane
         
+        return max(max_kadane, total - min_kadane)
