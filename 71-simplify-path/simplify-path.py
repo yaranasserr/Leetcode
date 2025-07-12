50a99e42ad4f7836c66afcf0893f1e034884a2a0
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-12 22:39:08 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (96.14%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,7 +1,8 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
-        path = path.split("/")
+        path = path.split("/") #['', 'home', 'user', 'Documents', '..', 'Pictures']
+        print(path)
 
         for w in path :
             if w == "" or w ==".":
