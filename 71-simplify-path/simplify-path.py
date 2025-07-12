7e686daf8e4fbe530544819a9edfe7c5ebed092f
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-12 22:37:50 +0300

    Time: 3 ms (37.30%) | Memory: 17.8 MB (80.30%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,22 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        path = path.split("/")
+
+        for w in path :
+            if w == "" or w ==".":
+                continue
+            if w == "..":
+                if stack:
+                    stack.pop()
+
+            else:
+                stack.append(w)
+
+        return "/" +"/".join(stack)
+
+            
+            
+
+
+        
