3bb303170e9122d35bc90eb8513528338f02bb44
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-13 16:59:32 +0300

    File updated at 7/13/2025, 4:59:32 PM

--- 228-SummaryRanges/228-SummaryRanges.py
+++ 228-SummaryRanges/228-SummaryRanges.py
@@ -1,41 +1,26 @@
-# Last updated: 7/13/2025, 4:58:26 PM
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
+# Last updated: 7/13/2025, 4:59:32 PM
+from collections import deque
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root :return None
+        if not root:
+            return None  
 
-        q=deque([root])
-      
+        q = deque([root])  
 
-        while q :
-            prev = None
-           
-            for i in range(len(q)):
-                node = q.popleft()
-                if prev :
-                    prev.next = node 
-                prev = node 
-                if node.left :
-                    q.append(node.left)
+        while q:
+            prev = None  # Reset previous node for each level
+
+            for _ in range(len(q)):  
+                node = q.popleft()  
+
+                if prev:
+                    prev.next = node  # Connect previous node's next to current node
+                prev = node  # Update previous node
+
+                if node.left:
+                    q.append(node.left) 
                 if node.right:
+                    q.append(node.right)  
 
-                    q.append(node.right)
-
-            if prev :
-                prev.next = None
-
-           
-
-        return root 
-
-                
-        
+        return root  
