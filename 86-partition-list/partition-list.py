3fd322fc0d070ff3934d6346d3f9e771c4642a09
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-13 14:19:01 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (66.71%) - LeetSync

---
+++ 86-partition-list/partition-list.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        #dummy nodes pointing to the heads of these new lists.
+        small_dummy = ListNode(0)
+        big_dummy = ListNode(0)
+        small = small_dummy
+        big = big_dummy
+
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                big.next = head
+                big = big.next
+            head = head.next
+
+        big.next = None       
+        small.next = big_dummy.next  # Connect the two partitions
+
+        return small_dummy.next
