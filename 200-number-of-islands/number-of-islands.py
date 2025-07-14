9e3a7e13c404d15699421fdcd5c87b69ca02a9dc
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-14 15:24:43 +0300

    Time: 238 ms (75.18%) | Memory: 20.1 MB (68.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,31 +1,42 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0 
+        m , n = len(grid) , len(grid[0])
 
-        if not grid:
-            return 0 
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
 
-        rows , cols = len(grid), len(grid[0])
-        island_count =0
-            
+        def dfs(r,c):
+            if r < 0 or c<0 or r >= m or c>= n or grid[r][c] == "0":
+                return 
 
-        def dfs(r, c):
-            # Check bounds and whether the cell is land
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
-                return
-
-            # Mark the cell as visited by changing it to water ('0')
             grid[r][c] = "0"
 
-            # Visit all 4 adjacent cells (up, down, left, right)
-            dfs(r + 1, c)  # down
-            dfs(r - 1, c)  # up
-            dfs(r, c + 1)  # right
-            dfs(r, c - 1)  # left
+            for dr ,dc in directions :
+                dfs(dr+r,dc+c)
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1":
-                    island_count += 1
-                    dfs(i, j)  # Start DFS to mark the entire island as visited
 
-        return island_count
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] =="1":
+                    islands +=1
+                    dfs(i,j)
+
+        return islands  
+
+        
+
+
+
+
+
+        
+
+
+
+
+            
+
+
+        
+        
