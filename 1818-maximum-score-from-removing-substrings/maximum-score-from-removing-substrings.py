c6fd4e8f5b4258f55e8a89da74c75a39b8e38bf3
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-23 14:09:08 +0300

    Time: 240 ms (47.74%) | Memory: 19.5 MB (50.97%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+
+        def remove(s, pair, score):
+            stack = []
+            total = 0
+            for ch in s:
+                if stack and stack[-1] == pair[0] and ch == pair[1]:
+                    stack.pop()
+                    total += score
+                else:
+                    stack.append(ch)
+            return "".join(stack), total
+
+  
+        if x > y:
+            s, points1 = remove(s, "ab", x)
+            _, points2 = remove(s, "ba", y)
+        else:
+            s, points1 = remove(s, "ba", y)
+            _, points2 = remove(s, "ab", x)
+
+        return points1 + points2
