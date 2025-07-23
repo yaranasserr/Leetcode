4874919466973f6d4a2380563c563f387a4269c0
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-23 18:21:03 +0300

    File updated at 7/23/2025, 6:21:03 PM

--- 64-MinimumPathSum/64-MinimumPathSum.py
+++ 64-MinimumPathSum/64-MinimumPathSum.py
@@ -1,18 +1,31 @@
-# Last updated: 7/23/2025, 3:56:41 PM
+# Last updated: 7/23/2025, 6:21:03 PM
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        res = []
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        memo = {}
+        def find(r,c):
+            if r>= m or c >= n :
+                return float("inf")
+
+
+            if r == m-1 and c== n-1 :
+                return grid[r][c]
+
+
+            if (r,c) in memo :
+                return memo[(r,c)]
+
+
+            memo[(r,c)] = grid[r][c] + min(find(r+1,c),find(r,c+1))
+
+            return memo[(r,c)]
+
+
+        return find(0,0)
         
-        def backtrack(i, comb):
-            if i > n:
-                if len(comb) == k:
-                    res.append(comb.copy())
-                return
-            
-            comb.append(i)
-            backtrack(i + 1, comb)
-            comb.pop()
-            backtrack(i + 1, comb)
+
+
+
         
-        backtrack(1, [])
-        return res
