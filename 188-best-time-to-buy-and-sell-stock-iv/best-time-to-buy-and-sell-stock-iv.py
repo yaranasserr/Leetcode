73c56ac6e8709dc30d3885914fce0684a075d115
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 16:49:35 +0300

    Time: 338 ms (5.03%) | Memory: 84.4 MB (5.19%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        dp={}
+        def solve(i,flag,count):
+            if i>=len(prices) or count>=k:
+                return 0
+            if (i,flag,count) in dp:
+                return dp[(i,flag,count)]
+            if flag:
+                buy=-prices[i]+solve(i+1,False,count)
+                skip=solve(i+1,True,count)
+                dp[(i,flag,count)]=max(buy,skip)
+            else:
+                sell=prices[i]+solve(i+1,True,count+1)
+                hold=solve(i+1,False,count)
+                dp[(i,flag,count)]=max(sell,hold)
+            return dp[(i,flag,count)]
+        return solve(0,True,0)
