fe40c764baeeb40e01f0008842ec3d2dc15c611e
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 14:16:16 +0300

    File updated at 7/27/2025, 2:16:16 PM

--- 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).py
+++ 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).py
@@ -1,29 +1,31 @@
-# Last updated: 7/27/2025, 2:13:42 PM
-"""
-# Definition for a QuadTree node.
-class Node:
-    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
-        self.val = val
-        self.isLeaf = isLeaf
-        self.topLeft = topLeft
-        self.topRight = topRight
-        self.bottomLeft = bottomLeft
-        self.bottomRight = bottomRight
-"""
+# Last updated: 7/27/2025, 2:16:16 PM
+import heapq
 
 class Solution:
-    def construct(self, grid: list[list[int]]) -> 'Node':
-        n = len(grid)
-        def build(r, c, l):
-            v = grid[r][c]
-            for i in range(r, r+l):
-                for j in range(c, c+l):
-                    if grid[i][j] != v:
-                        h = l//2
-                        tl = build(r, c, h)
-                        tr = build(r, c+h, h)
-                        bl = build(r+h, c, h)
-                        br = build(r+h, c+h, h)
-                        return Node(True, False, tl, tr, bl, br)
-            return Node(bool(v), True, None, None, None, None)
-        return build(0, 0, n)
+    def findMaximizedCapital(self, k: int, w: int, profits: list[int], capital: list[int]) -> int:
+        # Step 1: Create a list of projects (capital, profit)
+        projects = list(zip(capital, profits))
+        
+        # Step 2: Create a min-heap sorted by capital
+        min_capital_heap = []
+        for c, p in projects:
+            heapq.heappush(min_capital_heap, (c, p))
+        
+        # Step 3: Max-heap for available projects (use negative profits because Python heapq is a min-heap)
+        max_profit_heap = []
+        
+        # Step 4: Try to select up to k projects
+        for _ in range(k):
+            # Move all projects that can be afforded to max-profit heap
+            while min_capital_heap and min_capital_heap[0][0] <= w:
+                c, p = heapq.heappop(min_capital_heap)
+                heapq.heappush(max_profit_heap, -p)
+            
+            # If no project can be afforded, stop
+            if not max_profit_heap:
+                break
+            
+            # Do the most profitable project
+            w += -heapq.heappop(max_profit_heap)
+        
+        return w
