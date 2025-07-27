eedaa6fe157e8e2bbd010740b2b2600a3ba82a81
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 14:05:02 +0300

    Time: 7 ms (90.12%) | Memory: 17.7 MB (87.76%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        counter = 0
+
+        def backtrack(r, cols, diagonals, antidiagonals):
+            nonlocal counter
+            if r == n:
+                counter += 1
+                return
+            for c in range(n):
+                if c in cols or (r + c) in antidiagonals or (r - c) in diagonals:
+                    continue
+                cols.add(c)
+                antidiagonals.add(r + c)
+                diagonals.add(r - c)
+                backtrack(r + 1, cols, diagonals, antidiagonals)
+                cols.remove(c)
+                antidiagonals.remove(r + c)
+                diagonals.remove(r - c)
+
+        backtrack(0, set(), set(), set())
+        return counter
