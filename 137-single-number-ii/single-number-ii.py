e6a91b760302e85ecc3c1aec251802ab9e615dc8
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 16:46:41 +0300

    Time: 27 ms (33.37%) | Memory: 19.1 MB (88.49%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0
+        for i in range(32):
+            bit_sum = 0
+            for num in nums:
+                bit_sum += (num >> i) & 1
+            if bit_sum % 3:
+                result |= (1 << i)
+        if result >= 2**31:
+            result -= 2**32
+        return result
