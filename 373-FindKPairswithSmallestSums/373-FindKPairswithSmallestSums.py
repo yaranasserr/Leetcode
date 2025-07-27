265d7db850aa81dc6eb18ea4638ffabfdcb816b5
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 16:46:36 +0300

    File updated at 7/27/2025, 4:46:36 PM

--- 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.py
+++ 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.py
@@ -1,10 +1,13 @@
-# Last updated: 7/27/2025, 4:44:03 PM
+# Last updated: 7/27/2025, 4:46:36 PM
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        res = 0
-
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0
         for i in range(32):
-            if (n >> i) & 1:
-                res += 1
-
-        return res
+            bit_sum = 0
+            for num in nums:
+                bit_sum += (num >> i) & 1
+            if bit_sum % 3:
+                result |= (1 << i)
+        if result >= 2**31:
+            result -= 2**32
+        return result
