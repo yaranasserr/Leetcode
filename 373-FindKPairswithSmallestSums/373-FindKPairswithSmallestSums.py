fdaf763a006cde40735c337870ff8b7efd58930b
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 16:46:55 +0300

    File updated at 7/27/2025, 4:46:54 PM

--- 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.py
+++ 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.py
@@ -1,13 +1,6 @@
-# Last updated: 7/27/2025, 4:46:36 PM
+# Last updated: 7/27/2025, 4:46:54 PM
 class Solution:
-    def singleNumber(self, nums: list[int]) -> int:
-        result = 0
-        for i in range(32):
-            bit_sum = 0
-            for num in nums:
-                bit_sum += (num >> i) & 1
-            if bit_sum % 3:
-                result |= (1 << i)
-        if result >= 2**31:
-            result -= 2**32
-        return result
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while (right > left):  # Loop until right is no longer greater than left
+            right = right & (right - 1) # Clear the least significant bit of right
+        return right & left    # Return the bitwise AND of the modified right and left
