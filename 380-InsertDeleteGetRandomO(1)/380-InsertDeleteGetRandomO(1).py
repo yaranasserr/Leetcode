cef3339d4351f3be2522d545dce28c6c3d526037
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-27 14:04:58 +0300

    File updated at 7/27/2025, 2:04:58 PM

--- 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).py
+++ 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).py
@@ -1,21 +1,23 @@
-# Last updated: 7/27/2025, 2:02:18 PM
+# Last updated: 7/27/2025, 2:04:58 PM
 class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        l = len(words[0])
-        total = len(words) * l
+    def totalNQueens(self, n: int) -> int:
+        counter = 0
 
-        def frequency_map(word_list):
-            freq = {}
-            for ch in word_list:
-                freq[ch] = freq.get(ch, 0) + 1
-            return freq
+        def backtrack(r, cols, diagonals, antidiagonals):
+            nonlocal counter
+            if r == n:
+                counter += 1
+                return
+            for c in range(n):
+                if c in cols or (r + c) in antidiagonals or (r - c) in diagonals:
+                    continue
+                cols.add(c)
+                antidiagonals.add(r + c)
+                diagonals.add(r - c)
+                backtrack(r + 1, cols, diagonals, antidiagonals)
+                cols.remove(c)
+                antidiagonals.remove(r + c)
+                diagonals.remove(r - c)
 
-        main = frequency_map(words)
-        result = []
-
-        for start in range(0, len(s) - total + 1):
-            s_parts = [s[i:i + l] for i in range(start, start + total, l)]
-            if frequency_map(s_parts) == main:
-                result.append(start)
-
-        return result
+        backtrack(0, set(), set(), set())
+        return counter
