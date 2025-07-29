b2069789871e81ed5ee731ddeb61b46edd5b7311
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-29 21:40:54 +0300

    File updated at 7/29/2025, 9:40:54 PM

--- 64-MinimumPathSum/64-MinimumPathSum.py
+++ 64-MinimumPathSum/64-MinimumPathSum.py
@@ -1,31 +1,29 @@
-# Last updated: 7/23/2025, 6:21:03 PM
+# Last updated: 7/29/2025, 9:40:54 PM
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        # top down 
         m = len(grid)
         n = len(grid[0])
+        dp ={}
 
-        memo = {}
-        def find(r,c):
-            if r>= m or c >= n :
+        def dfs(r,c):
+            if r <0 or c < 0 or r>= m or c>=n :
                 return float("inf")
 
-
-            if r == m-1 and c== n-1 :
+            if r == m-1 and c == n-1:
                 return grid[r][c]
 
+            if (r,c) in dp :
+                return dp[(r,c)]
 
-            if (r,c) in memo :
-                return memo[(r,c)]
+            dp[(r,c)] = grid[r][c]+min(dfs(r+1,c),dfs(r,c+1))
 
+            return dp[(r,c)]
 
-            memo[(r,c)] = grid[r][c] + min(find(r+1,c),find(r,c+1))
-
-            return memo[(r,c)]
-
-
-        return find(0,0)
-        
+        return dfs(0,0)
+       
+        # Time Complexity: O(m * n)
 
 
-
+# Without memoization, the recursion would branch into two calls at each step (down and right), leading to an exponential number of calls: O(2^(m+n)).
         
