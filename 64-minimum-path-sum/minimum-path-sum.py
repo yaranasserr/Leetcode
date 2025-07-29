79a3d91dbdf631fc6e0a94feb01fe67585244360
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-29 21:40:58 +0300

    Time: 63 ms (6.81%) | Memory: 28.3 MB (6.43%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,37 +1,28 @@
-from typing import List
-
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        #dynamic programming solution  bottom up 
+        # top down 
+        m = len(grid)
+        n = len(grid[0])
+        dp ={}
 
-        res =[[float("inf")] * (cols+1) for r in range(rows+1)]
+        def dfs(r,c):
+            if r <0 or c < 0 or r>= m or c>=n :
+                return float("inf")
 
-        res[rows-1][cols]=0
-        for r in range(rows-1,-1,-1):
-            for c in range(cols-1,-1,-1):
-                res[r][c] = grid[r][c] + min(res[r+1][c],res[r][c+1])
-        return res[0][0]
+            if r == m-1 and c == n-1:
+                return grid[r][c]
 
-        #recusrion
-        # memo = {}
+            if (r,c) in dp :
+                return dp[(r,c)]
 
-        # def find(r, c):
-        #     # If out of bounds, return infinity (invalid path)
-        #     if r >= rows or c >= cols:
-        #         return float('inf')
-            
-        #     # If at the bottom-right corner, return its value
-        #     if r == rows - 1 and c == cols - 1:
-        #         return grid[r][c]
+            dp[(r,c)] = grid[r][c]+min(dfs(r+1,c),dfs(r,c+1))
 
-        #     # If already computed, return the stored value
-        #     if (r, c) in memo:
-        #         return memo[(r, c)]
+            return dp[(r,c)]
 
-        #     # Compute the minimum path sum recursively
-        #     memo[(r, c)] = grid[r][c] + min(find(r + 1, c), find(r, c + 1))
-        #     return memo[(r, c)]
+        return dfs(0,0)
+       
+        # Time Complexity: O(m * n)
 
-        # return find(0, 0)
+
+# Without memoization, the recursion would branch into two calls at each step (down and right), leading to an exponential number of calls: O(2^(m+n)).
+        
