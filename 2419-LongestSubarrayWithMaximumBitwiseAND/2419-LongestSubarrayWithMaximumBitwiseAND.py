a6b955d462ac1a7df00c9141cecd8dd0711224a4
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-07-30 14:11:22 +0300

    Create 2419-LongestSubarrayWithMaximumBitwiseAND

---
+++ 2419-LongestSubarrayWithMaximumBitwiseAND/2419-LongestSubarrayWithMaximumBitwiseAND.py
@@ -0,0 +1,16 @@
+# Last updated: 7/30/2025, 2:11:19 PM
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_val = ans = current_streak = 0
+        for num in nums:
+            if max_val < num:
+                max_val = num
+                ans = current_streak = 0
+
+            if max_val == num:
+                current_streak += 1
+            else:
+                current_streak = 0
+
+            ans = max(ans, current_streak)
+        return ans
