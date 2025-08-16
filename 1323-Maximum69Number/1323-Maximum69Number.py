10dc5fcca751b637216f32745b9346d8964c8f98
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-08-16 16:20:36 +0300

    Create 1323-Maximum69Number

---
+++ 1323-Maximum69Number/1323-Maximum69Number.py
@@ -0,0 +1,9 @@
+# Last updated: 8/16/2025, 4:20:36 PM
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num_str = list(str(num))   # convert int -> list of chars
+        for i in range(len(num_str)):
+            if num_str[i] == '6':  # change first '6' to '9'
+                num_str[i] = '9'
+                break
+        return int("".join(num_str))
