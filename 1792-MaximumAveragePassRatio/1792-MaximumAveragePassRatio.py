56722b07c43d6254658fef503b70d3dce1653936
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-01 15:46:46 +0300

    Create 1792-MaximumAveragePassRatio

---
+++ 1792-MaximumAveragePassRatio/1792-MaximumAveragePassRatio.py
@@ -0,0 +1,26 @@
+# Last updated: 9/1/2025, 3:46:46 PM
+import heapq
+
+class Solution:
+    def maxAverageRatio(self, classes: list[list[int]], extraStudents: int) -> float:
+        pq = []
+        for p, t in classes:
+            gain = self.findGain(p, t)
+            heapq.heappush(pq, (-gain, p, t))
+        
+        while extraStudents > 0:
+            gain, p, t = heapq.heappop(pq)
+            p += 1
+            t += 1
+            new_gain = self.findGain(p, t)
+            heapq.heappush(pq, (-new_gain, p, t))
+            extraStudents -= 1
+        
+        res = 0.0
+        while pq:
+            _, p, t = heapq.heappop(pq)
+            res += p / t
+        return res / len(classes)
+    
+    def findGain(self, p: int, t: int) -> float:
+        return (p + 1) / (t + 1) - p / t
