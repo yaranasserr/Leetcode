4ba6872c30ca852eb049d5f90c5774bf6cc33255
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-05 17:26:47 +0300

    Create 2749-MinimumOperationstoMaketheIntegerZero

---
+++ 2749-MinimumOperationstoMaketheIntegerZero/2749-MinimumOperationstoMaketheIntegerZero.py
@@ -0,0 +1,11 @@
+# Last updated: 9/5/2025, 5:26:47 PM
+class Solution:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        k = 1
+        while True:
+            x = num1 - num2 * k
+            if x < k:
+                return -1
+            if k >= x.bit_count():
+                return k
+            k += 1
