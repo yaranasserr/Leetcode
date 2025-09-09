3f4cebbbc6d912f0c932b1ab5f124c7b0f663d7b
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-09 14:48:04 +0300

    Create 2327-NumberofPeopleAwareofaSecret

---
+++ 2327-NumberofPeopleAwareofaSecret/2327-NumberofPeopleAwareofaSecret.py
@@ -0,0 +1,18 @@
+# Last updated: 9/9/2025, 2:48:04 PM
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        knows = [0] * n
+        knows[0] = 1
+        shared, total = 0, 1
+
+        for day in range(delay, forget):
+            shared += knows[day - delay]
+            total += shared
+            knows[day] = shared
+
+        for day in range(forget, n):
+            shared += knows[day - delay] - knows[day - forget]
+            total += shared - knows[day - forget]
+            knows[day] = shared
+
+        return total % 1_000_000_007
