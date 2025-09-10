7597fe38bf6faa3bd048303a2614adb6cfbba115
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-10 15:16:07 +0300

    Time: 60 ms (74.39%) | Memory: 32 MB (69.51%) - LeetSync

---
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        known = [set(ls) for ls in languages]
+        need = set()
+
+        for u, v in friendships:
+            u -= 1; v -= 1
+            if known[u] & known[v]:
+                continue
+            need.add(u); need.add(v)
+
+        if not need:
+            return 0
+
+        ans = float('inf')
+        for lang in range(1, n + 1):
+            cnt = sum(1 for person in need if lang not in known[person])
+            ans = min(ans, cnt)
+        return ans
