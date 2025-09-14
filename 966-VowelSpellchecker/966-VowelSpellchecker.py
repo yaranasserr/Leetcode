c242725c5f9b531166ac8e4a922b7a0db904780e
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-14 12:37:23 +0300

    Create 966-VowelSpellchecker

---
+++ 966-VowelSpellchecker/966-VowelSpellchecker.py
@@ -0,0 +1,32 @@
+# Last updated: 9/14/2025, 12:37:23 PM
+class Solution:
+    def spellchecker(self, wordlist, queries):
+        exact = set(wordlist)
+        caseMap = {}
+        vowelMap = {}
+
+        for w in wordlist:
+            lower = w.lower()
+            devowel = self.deVowel(lower)
+            if lower not in caseMap:
+                caseMap[lower] = w
+            if devowel not in vowelMap:
+                vowelMap[devowel] = w
+
+        result = []
+        for q in queries:
+            if q in exact:
+                result.append(q)
+            else:
+                lower = q.lower()
+                devowel = self.deVowel(lower)
+                if lower in caseMap:
+                    result.append(caseMap[lower])
+                elif devowel in vowelMap:
+                    result.append(vowelMap[devowel])
+                else:
+                    result.append("")
+        return result
+
+    def deVowel(self, s):
+        return ''.join('*' if c in 'aeiou' else c for c in s)
