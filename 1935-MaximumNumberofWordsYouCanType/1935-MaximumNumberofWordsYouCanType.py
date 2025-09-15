5d3554c061575d70561d552da3a0e150c3355a98
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-15 12:04:26 +0300

    File updated at 9/15/2025, 12:04:26 PM

--- 1935-MaximumNumberofWordsYouCanType/1935-MaximumNumberofWordsYouCanType.py
+++ 1935-MaximumNumberofWordsYouCanType/1935-MaximumNumberofWordsYouCanType.py
@@ -1,8 +1,6 @@
-# Last updated: 9/15/2025, 12:03:06 PM
+# Last updated: 9/15/2025, 12:04:26 PM
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        words = text.split()
-        brokenLetters = list(brokenLetters)
 
-        count = sum(1 for word in words if any(ch in word for ch in brokenLetters))
-        return len(words) - count
+        return len(text.split())- sum(1 for word in text.split() if any(ch in word for ch in list(brokenLetters)))
+ 
