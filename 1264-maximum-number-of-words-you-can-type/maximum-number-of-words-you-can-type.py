915e6afcfef3a15fa8d1f624ea0989eae4b409ac
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-15 12:03:10 +0300

    Time: 3 ms (36.59%) | Memory: 17.8 MB (84.48%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        words = text.split()
+        brokenLetters = list(brokenLetters)
+
+        count = sum(1 for word in words if any(ch in word for ch in brokenLetters))
+        return len(words) - count
