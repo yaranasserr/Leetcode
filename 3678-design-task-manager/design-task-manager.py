9f7d4ac6e053bc0484896ff12014ea648e1e7533
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-18 13:54:48 +0300

    Time: 431 ms (92.83%) | Memory: 104 MB (59.62%) - LeetSync

---
+++ 3678-design-task-manager/design-task-manager.py
@@ -0,0 +1,46 @@
+class TaskManager:
+
+    def __init__(self, tasks: List[List[int]]):
+        self.current_tasks = {} # taskid-> p, user 
+        self.heap = []
+        for u,t,p in tasks:
+            self.current_tasks[t] =(p,u)
+            heapq.heappush(self.heap,(-p,-t,u)) # max heap
+        
+
+    def add(self, userId: int, taskId: int, priority: int) -> None:
+        self.current_tasks[taskId]=(priority,userId)
+        heapq.heappush(self.heap,(-priority,-taskId,userId))
+        
+
+    def edit(self, taskId: int, newPriority: int) -> None:
+        current_p , current_u = self.current_tasks[taskId]
+        self.current_tasks[taskId] = (newPriority,current_u)
+        heapq.heappush(self.heap,(-newPriority,-taskId,current_u))
+
+
+    def rmv(self, taskId: int) -> None:
+        del self.current_tasks[taskId]
+        
+
+    def execTop(self) -> int:
+        while self.heap :
+            p,t,u = heapq.heappop(self.heap)
+            p,t = abs(p),abs(t)
+
+            if t not in self.current_tasks or self.current_tasks[t][0] != p or self.current_tasks[t][1] != u :
+                continue 
+
+            del self.current_tasks[t]
+            return u 
+
+        return -1 
+        
+
+
+# Your TaskManager object will be instantiated and called as such:
+# obj = TaskManager(tasks)
+# obj.add(userId,taskId,priority)
+# obj.edit(taskId,newPriority)
+# obj.rmv(taskId)
+# param_4 = obj.execTop()
