e28336b829aa006a7a9c613cceb6c088b8901df1
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-18 13:54:46 +0300

    File updated at 9/18/2025, 1:54:46 PM

--- 3408-DesignTaskManager/3408-DesignTaskManager.py
+++ 3408-DesignTaskManager/3408-DesignTaskManager.py
@@ -1,12 +1,12 @@
+# Last updated: 9/18/2025, 1:54:46 PM
 class TaskManager:
 
     def __init__(self, tasks: List[List[int]]):
-        self.current_tasks={}
-        self.heap= []
+        self.current_tasks = {} # taskid-> p, user 
+        self.heap = []
         for u,t,p in tasks:
-            self.current_tasks[t]=(p,u)
-            heapq.heappush(self.heap,(-p,-t,u))
-
+            self.current_tasks[t] =(p,u)
+            heapq.heappush(self.heap,(-p,-t,u)) # max heap
         
 
     def add(self, userId: int, taskId: int, priority: int) -> None:
@@ -14,33 +14,28 @@ class TaskManager:
         heapq.heappush(self.heap,(-priority,-taskId,userId))
         
 
-
     def edit(self, taskId: int, newPriority: int) -> None:
-        currentpriority,current_userid=self.current_tasks[taskId] # get p , u from dict 
-        self.current_tasks[taskId] = (newPriority,current_userid)
-        heapq.heappush(self.heap,(-newPriority,-taskId,current_userid))
+        current_p , current_u = self.current_tasks[taskId]
+        self.current_tasks[taskId] = (newPriority,current_u)
+        heapq.heappush(self.heap,(-newPriority,-taskId,current_u))
 
-
-
-
-        
 
     def rmv(self, taskId: int) -> None:
         del self.current_tasks[taskId]
+        
 
     def execTop(self) -> int:
-        while self.heap:
-            priority, taskId, userId = heapq.heappop(self.heap)
-            priority, taskId = abs(priority), abs(taskId)
+        while self.heap :
+            p,t,u = heapq.heappop(self.heap)
+            p,t = abs(p),abs(t)
 
-            if taskId not in self.current_tasks or self.current_tasks[taskId][0] != priority or self.current_tasks[taskId][1] != userId:
-                continue
+            if t not in self.current_tasks or self.current_tasks[t][0] != p or self.current_tasks[t][1] != u :
+                continue 
 
-            del self.current_tasks[taskId]
-            return userId
+            del self.current_tasks[t]
+            return u 
 
-        return -1
-
+        return -1 
         
 
 
