02ff4b69eb6133680116bd6fe6c87e1487f94ff3
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-09-22 12:31:39 +0300

    File updated at 9/22/2025, 12:31:39 PM

--- 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
+++ 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
@@ -1,11 +1,7 @@
-# Last updated: 9/22/2025, 12:29:25 PM
+# Last updated: 9/22/2025, 12:31:39 PM
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         # get frequency 
         # get the numbers of maximum frequency 
-        num_count=Counter(nums)
 
-        maximum = max(num_count.values()) # maximum frequency
-        count_maximum = list(num_count.values()).count(maximum) # count of max frequency 
-
-        return maximum*count_maximum
+        return max(Counter(nums).values()) * list(Counter(nums).values()).count(max(Counter(nums).values()))
