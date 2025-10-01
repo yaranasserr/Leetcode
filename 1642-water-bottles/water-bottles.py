f26f3b315dc9dd237451104d83703bf5aae7fd23
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-01 12:16:43 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (17.52%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        cons = 0 
+        while  numBottles >= numExchange :
+            cons += numExchange
+            numBottles -=numExchange
+
+            numBottles +=1 # exchange them for one full bottle
+
+
+        return numBottles + cons
+        
