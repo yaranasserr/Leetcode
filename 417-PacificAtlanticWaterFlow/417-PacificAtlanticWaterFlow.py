4abf4ea1faeb50186b41a360e874bfaaee35cf88
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-05 17:20:17 +0300

    File updated at 10/5/2025, 5:20:17 PM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,44 +1,38 @@
-import collections
+# Last updated: 10/5/2025, 5:20:17 PM
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows = len(heights)
-        cols= len(heights[0])
+        atl , pac = set(),set()
+        m,n = len(heights),len(heights[0])
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        res = []
+     
 
-        def dfs(r,c,visit,prevheight):
-            if(r,c) in visit or  r< 0 or c<0 or r== rows or c == cols or heights[r][c] < prevheight:
-                return 
+        def dfs(r,c,visit,prevh):
+            if r <0 or c<0 or r >= m or c>=n or (r,c) in visit or heights[r][c]< prevh:
+                return
             visit.add((r,c))
 
-            dfs(r+1,c,visit,heights[r][c])
-            dfs(r-1,c,visit,heights[r][c])
-            dfs(r,c+1,visit,heights[r][c])
-            dfs(r,c-1,visit,heights[r][c])
 
+            for dr , dc in directions :
+                dfs(r+dr,c+dc,visit,heights[r][c])
 
-        pac , atl = set() , set()
-        # every (column) positon in first row
-        for c in range(cols):
-            dfs(0,c,pac,heights[0][c])  # first row
-            dfs(rows-1,c,atl,heights[rows-1][c]) # last row 
+        
+        for r in range(m):
+            dfs(r,0,pac,heights[r][0]) # left , pac 
+            dfs(r,n-1,atl,heights[r][n-1]) # right  .,atl
 
-        for r in range(rows):
-            dfs(r,0,pac,heights[r][0]) # first column
-            dfs(r,cols-1,atl,heights[r][cols-1]) # lst column
+        for c in range(n):
+            dfs(0,c,pac,heights[0][c]) # top , pac
+            dfs(m-1,c,atl,heights[m-1][c]) # bottom ,atl
 
-        res=[]
-
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) in pac and (r,c) in atl:
+        for r in range(m):
+            for c in range(n):
+                if (r,c) in pac and (r,c) in atl :
                     res.append([r,c])
+
         return res
 
+        
 
 
-
-
-
-
-
-      
-       
+        
