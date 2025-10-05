65ae67b3c15b30e18c394c38e3769092e074a090
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-05 17:21:32 +0300

    File updated at 10/5/2025, 5:21:31 PM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,38 +1,21 @@
-# Last updated: 10/5/2025, 5:20:17 PM
+# Last updated: 10/5/2025, 5:21:31 PM
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        atl , pac = set(),set()
-        m,n = len(heights),len(heights[0])
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
-        res = []
-     
+        m, n = len(heights), len(heights[0])
+        pac, atl = set(), set()
 
-        def dfs(r,c,visit,prevh):
-            if r <0 or c<0 or r >= m or c>=n or (r,c) in visit or heights[r][c]< prevh:
+        def dfs(r, c, visit, prev):
+            if (r, c) in visit or not (0 <= r < m and 0 <= c < n) or heights[r][c] < prev:
                 return
-            visit.add((r,c))
+            visit.add((r, c))
+            for dr, dc in ((1,0),(-1,0),(0,1),(0,-1)):
+                dfs(r+dr, c+dc, visit, heights[r][c])
 
+        for i in range(m):
+            dfs(i, 0, pac, heights[i][0])
+            dfs(i, n-1, atl, heights[i][n-1])
+        for j in range(n):
+            dfs(0, j, pac, heights[0][j])
+            dfs(m-1, j, atl, heights[m-1][j])
 
-            for dr , dc in directions :
-                dfs(r+dr,c+dc,visit,heights[r][c])
-
-        
-        for r in range(m):
-            dfs(r,0,pac,heights[r][0]) # left , pac 
-            dfs(r,n-1,atl,heights[r][n-1]) # right  .,atl
-
-        for c in range(n):
-            dfs(0,c,pac,heights[0][c]) # top , pac
-            dfs(m-1,c,atl,heights[m-1][c]) # bottom ,atl
-
-        for r in range(m):
-            for c in range(n):
-                if (r,c) in pac and (r,c) in atl :
-                    res.append([r,c])
-
-        return res
-
-        
-
-
-        
+        return [[r, c] for r in range(m) for c in range(n) if (r, c) in pac & atl]
