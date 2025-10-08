34499636c24e498b9b422ed45d02316f3265ac56
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-08 14:39:03 +0300

    File updated at 10/8/2025, 2:39:03 PM

--- 2300-SuccessfulPairsofSpellsandPotions/2300-SuccessfulPairsofSpellsandPotions.py
+++ 2300-SuccessfulPairsofSpellsandPotions/2300-SuccessfulPairsofSpellsandPotions.py
@@ -1,57 +1,26 @@
-from typing import List
-
+# Last updated: 10/8/2025, 2:39:03 PM
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        potions.sort()  # Sort potions for binary search
+        potions.sort()
+        res = []
         n = len(potions)
-        result = []
-        
-        def binary_search(spell: int) -> int:
-            left, right = 0, n - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if spell * potions[mid] >= success:
-                    right = mid - 1  # Look for a smaller index
+
+        def binary_search(a):
+            l , r = 0, n-1
+            while l<=r :
+                mid = (l+r)//2
+                if a*potions[mid]>= success:
+                    r = mid-1
                 else:
-                    left = mid + 1  # Look for a larger index
-            return left  # Left points to the first valid potion
-        
-        for spell in spells:
-            index = binary_search(spell)
-            result.append(n - index)  # Count successful pairs
-        
-        return result
+                    l = mid+1
+
+            return l # first valid potion
+
+        for s in spells:
+            idx = binary_search(s)
+            res.append(n-idx)
+
+        return res 
 
 
-# class Solution:
-#     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-#        potions.sort() 
-#        results = [[spell * potion for potion in potions] for spell in spells]
-#        res = []
-#        for row in results:
-        
-#         for i in range(len(row)) :
-#             l,r=0,len(row)-1
-#             while l <= r:
-#                 m= l+r //2 
-#                 if success > m:
-#                     l= m-1
-#                 elif success < m:
-#                     r = m+1
-#                 else:
-#                     return m 
-#         res.append(len(row)-m)
-
-#         return res 
-
-    
-
-
-
-        
-
-         
-
-
-        
         
