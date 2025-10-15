ee631b5041b87ae91c5fec90e4f0a1a149ad3b9f
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-15 14:35:50 +0300

    Create 3350-AdjacentIncreasingSubarraysDetectionII

---
+++ 3350-AdjacentIncreasingSubarraysDetectionII/3350-AdjacentIncreasingSubarraysDetectionII.py
@@ -0,0 +1,13 @@
+# Last updated: 10/15/2025, 2:35:49 PM
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        cnt, precnt, ans = 1, 0, 0
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                cnt += 1
+            else:
+                precnt, cnt = cnt, 1
+            ans = max(ans, min(precnt, cnt))
+            ans = max(ans, cnt // 2)
+        return ans
