d94ce0ce03aa06b278fcc82e6187e79bfde9428e
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-19 15:18:14 +0300

    Time: 496 ms (84.89%) | Memory: 30.6 MB (28.06%) - LeetSync

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+        # graph - dfs 
+        n = len(s)
+        incremented={str(n):str((n+a)%10) for n in range(10)} # for cycling
+        # {'0': '9', '1': '0', '2': '1', '3': '2', '4': '3', '5': '4', '6': '5', '7': '6', '8': '7', '9': '8'}
+
+        def add(s):
+            res = ""
+            for i in range(n):
+                res+= s[i] if i%2 == 0 else incremented[s[i]] # if even index , dont add , otherwise take the num from increment
+            return res 
+
+        def rotate(s):
+            return s[n-b:] +s[:n-b]
+
+        seen = set()
+
+        def dfs(s):
+            if s in seen :
+                return 
+
+            seen.add(s)
+            dfs(add(s))
+            dfs(rotate(s))
+
+            return 
+
+        dfs(s)
+
+        return min(seen)
+
+
+
+
+
+        
+        
