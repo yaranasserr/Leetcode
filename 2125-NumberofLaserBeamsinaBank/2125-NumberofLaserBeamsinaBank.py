ec7ef7ee66aba29349f7de2a52ebf3e942290565
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-27 11:13:10 +0300

    Create 2125-NumberofLaserBeamsinaBank

---
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
@@ -0,0 +1,11 @@
+# Last updated: 10/27/2025, 11:13:10 AM
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans=0
+        prev=0
+        for row in bank:
+            dev=row.count('1')
+            if dev>0:
+                ans+=dev*prev
+                prev=dev
+        return ans
