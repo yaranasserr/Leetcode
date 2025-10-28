cae9e44f2498eb84493d114292124fe429306889
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-28 13:16:36 +0300

    Create 3354-MakeArrayElementsEqualtoZero

---
+++ 3354-MakeArrayElementsEqualtoZero/3354-MakeArrayElementsEqualtoZero.py
@@ -0,0 +1,14 @@
+# Last updated: 10/28/2025, 1:16:35 PM
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        length = len(nums)
+        count = 0
+        left = 0
+        right = sum(nums)
+        for i in range(length):
+            left += nums[i]
+            right -= nums[i]
+            if nums[i] != 0: continue
+            if left == right: count += 2
+            if abs(left - right) == 1: count += 1
+        return count
