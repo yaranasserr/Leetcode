1fe5a4ca75868d7b0a154e901851335e5f4e1aa1
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-10-29 11:38:50 +0300

    Create 3370-SmallestNumberWithAllSetBits

---
+++ 3370-SmallestNumberWithAllSetBits/3370-SmallestNumberWithAllSetBits.py
@@ -0,0 +1,7 @@
+# Last updated: 10/29/2025, 11:38:50 AM
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        x = 1
+        while x < n:
+            x = x * 2 + 1
+        return x
