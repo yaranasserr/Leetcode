b3d15bfa3ac64c8bcc16cd705b23e55fef03e5a9
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-11-05 11:09:23 +0200

    Create 3321-FindX-SumofAllK-LongSubarraysII

---
+++ 3321-FindX-SumofAllK-LongSubarraysII/3321-FindX-SumofAllK-LongSubarraysII.py
@@ -0,0 +1,58 @@
+# Last updated: 11/5/2025, 11:09:23 AM
+from sortedcontainers import SortedList
+from collections import defaultdict
+
+class Solution:
+    def findXSum(self, nums, k, x):
+        numCount = defaultdict(int)
+        top = SortedList(key=lambda p: (-p[0], -p[1]))      # highest freq & value first
+        bottom = SortedList(key=lambda p: (-p[0], -p[1]))   # same ordering
+        result = []
+        running_sum = 0
+
+        for i, num in enumerate(nums):
+            count = numCount[num]
+
+            # Remove old count entry
+            if count > 0:
+                if (count, num) in top:
+                    top.remove((count, num))
+                    running_sum -= count * num
+                else:
+                    bottom.discard((count, num))
+
+            count += 1
+            numCount[num] = count
+            top.add((count, num))
+            running_sum += count * num
+
+            if len(top) > x:
+                smallest = top[-1]
+                running_sum -= smallest[0] * smallest[1]
+                bottom.add(smallest)
+                top.remove(smallest)
+
+            if i >= k:
+                old_num = nums[i - k]
+                old_count = numCount[old_num]
+                if (old_count, old_num) in top:
+                    running_sum -= old_count * old_num
+                    top.remove((old_count, old_num))
+                else:
+                    bottom.discard((old_count, old_num))
+
+                old_count -= 1
+                numCount[old_num] = old_count
+                if old_count > 0:
+                    bottom.add((old_count, old_num))
+
+                if len(top) < x and len(bottom) > 0:
+                    best = bottom[0]
+                    running_sum += best[0] * best[1]
+                    top.add(best)
+                    bottom.remove(best)
+
+            if i + 1 >= k:
+                result.append(running_sum)
+
+        return result
