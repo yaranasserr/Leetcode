46215af495d83adeef7a70ccd33ae52705d09e14
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-11-08 11:15:49 +0200

    Create 1611-MinimumOneBitOperationstoMakeIntegersZero

---
+++ 1611-MinimumOneBitOperationstoMakeIntegersZero/1611-MinimumOneBitOperationstoMakeIntegersZero.py
@@ -0,0 +1,13 @@
+# Last updated: 11/8/2025, 11:15:49 AM
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        k = 0
+        curr = 1
+        while (curr * 2) <= n:
+            curr *= 2
+            k += 1
+
+        return 2 ** (k + 1) - 1 - self.minimumOneBitOperations(n ^ curr)
