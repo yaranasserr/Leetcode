292768c02e4b815dd37506419c393172ada32599
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-11-10 15:09:31 +0200

    Create 3542-MinimumOperationstoConvertAllElementstoZero

---
+++ 3542-MinimumOperationstoConvertAllElementstoZero/3542-MinimumOperationstoConvertAllElementstoZero.py
@@ -0,0 +1,14 @@
+# Last updated: 11/10/2025, 3:09:30 PM
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        s = []
+        res = 0
+        for a in nums:
+            while s and s[-1] > a:
+                s.pop()
+            if a == 0:
+                continue
+            if not s or s[-1] < a:
+                res += 1
+                s.append(a)
+        return res
