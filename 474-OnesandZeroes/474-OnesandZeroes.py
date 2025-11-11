a8eb58b31e48f9343fddeb510d8ac89842a73fee
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2025-11-11 17:40:00 +0200

    Create 474-OnesandZeroes

---
+++ 474-OnesandZeroes/474-OnesandZeroes.py
@@ -0,0 +1,18 @@
+# Last updated: 11/11/2025, 5:39:59 PM
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = defaultdict(int)
+        
+        for s in strs:
+            zeros = s.count("0")
+            ones = s.count("1")
+            
+            # Use new loop vars to avoid overwriting m and n
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[(i, j)] = max(1 + dp[(i - zeros, j - ones)], dp[(i, j)])
+        
+        return dp[(m, n)]
