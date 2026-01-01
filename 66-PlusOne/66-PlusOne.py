9b1cbe7f50b6de11e1299f0db6da01ec1fee5196
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2026-01-01 12:14:11 +0200

    File updated at 1/1/2026, 12:14:11 PM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,22 +1,10 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        s=""
-        for i in digits:
-            s = s + str(i)
-            
-            
-        number = int(s)
-        number = number + 1
-        
-        result = []
-        
-        for c in str(number):
-            result.append(int(c))
-            
-        return result
-        
-            
+# Last updated: 1/1/2026, 12:14:11 PM
+1class Solution:
+2    def plusOne(self, digits: List[int]) -> List[int]:
+3        return [int(c) for c in str(int("".join(map(str, digits))) + 1)]
+4
+5        
+6
+7
+8
+9        
