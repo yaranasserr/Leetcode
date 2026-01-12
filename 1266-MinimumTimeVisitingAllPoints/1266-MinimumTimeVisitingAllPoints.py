8c80efbf69584cc511e9ff7c9957ea97fc64872d
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2026-01-12 12:28:16 +0200

    Create 1266-MinimumTimeVisitingAllPoints

---
+++ 1266-MinimumTimeVisitingAllPoints/1266-MinimumTimeVisitingAllPoints.py
@@ -0,0 +1,10 @@
+# Last updated: 1/12/2026, 12:28:16 PM
+1class Solution:
+2    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+3        ans = 0
+4        for i in range(len(points) - 1):
+5            curr_x, curr_y = points[i]
+6            target_x, target_y = points[i + 1]
+7            ans += max(abs(target_x - curr_x), abs(target_y - curr_y))
+8        
+9        return ans
