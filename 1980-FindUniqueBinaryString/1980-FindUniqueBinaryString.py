7a9ffd1235b4ab764bf277f8ea637decefa6f3a2
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2026-03-08 14:57:58 +0200

    File updated at 3/8/2026, 2:57:58 PM

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
@@ -1,17 +1,20 @@
-class Solution:
-    def findDifferentBinaryString(self, nums: List[str]) -> str:
-        n= len(nums[0])
-        string = "".join(map(str, [1]*(n))) 
-        deca = int(string, 2)
-        res = []
-        for i in range(deca+1):
-            res.append(i)
-
-        binary_list = [format(num, f'0{n}b') for num in res]
-        notin=[]
-        for b in binary_list:
-            if b not in nums:
-                notin.append(b)
-        
-        return notin[0]
-        
+# Last updated: 3/8/2026, 2:57:58 PM
+1# class Solution:
+2#     def findDifferentBinaryString(self, nums: List[str]) -> str:
+3#         n= len(nums[0])
+4#         string = "".join(map(str, [1]*(n))) 
+5#         deca = int(string, 2)
+6#         res = []
+7#         for i in range(deca+1):
+8#             res.append(i)
+9
+10#         binary_list = [format(num, f'0{n}b') for num in res]
+11#         notin=[]
+12#         for b in binary_list:
+13#             if b not in nums:
+14#                 notin.append(b)
+15        
+16#         return notin[0]
+17class Solution:
+18    def findDifferentBinaryString(self, nums: List[str]) -> str:
+19        return "".join("0" if nums[i][i] == "1" else "1" for i in range(len(nums)))
